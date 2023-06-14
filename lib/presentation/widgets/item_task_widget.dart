81dbcfffc4446ef7d4e25cb507e5480a0a2e7c93
Dmitrii Smirnov <sd@isu-it.ru>
2023-06-14 11:17:12 +0300

    no problem

--- lib/presentation/widgets/item_task_widget.dart
+++ lib/presentation/widgets/item_task_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
--- lib/presentation/widgets/list_tasks_widget.dart
+++ lib/presentation/widgets/list_tasks_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do_list/bloc/task_bloc.dart';
 import 'package:to_do_list/models/task.dart';
