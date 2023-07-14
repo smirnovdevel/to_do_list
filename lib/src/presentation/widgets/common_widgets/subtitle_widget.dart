1929774734e35a88fc80a1fbda2fa67b6d3c301f
Dmitrii Smirnov <smirnov.devel@gmail.com>
2023-07-14 09:58:40 +0300

    stop refactoring mobile resolution && orientation

---
+++ lib/src/presentation/widgets/common_widgets/subtitle_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../domain/models/todo.dart';
+import '../../core/localization/app_localization.dart';
+
+class SubtitleTodoWidget extends StatelessWidget {
+  const SubtitleTodoWidget({super.key, required this.todo});
+
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    final locale = AppLocalization.of(context).locale.languageCode;
+    if (todo.deadline == null) {
+      return Container();
+    }
+    return Text(
+      DateFormat('dd MMMM yyyy', locale)
+          .format(DateTime.fromMillisecondsSinceEpoch(todo.deadline!)),
+      style: todo.done
+          ? const TextStyle(color: Colors.grey)
+          : const TextStyle(color: Colors.blue),
+    );
+  }
+}
