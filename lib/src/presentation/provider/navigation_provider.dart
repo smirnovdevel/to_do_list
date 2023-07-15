8eb777edb56a86ebe29f8b1e2f35d830f9e07ab8
Dmitrii Smirnov <smirnov.devel@gmail.com>
2023-07-15 15:34:53 +0300

    change Navigator Provider

--- lib/src/presentation/provider/navigation_provider.dart
+++ lib/src/presentation/provider/navigation_provider.dart
@@ -2,7 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../navigation/router_delegate.dart';
 
-ChangeNotifierProvider<TodosRouterDelegate> navigationProvider =
-    ChangeNotifierProvider(
+Provider<TodosRouterDelegate> navigationProvider = Provider(
   (ref) => TodosRouterDelegate(ref),
 );
