a058be604b0097e1d58c271e940639e7c44aeae6
Dmitrii Smirnov <smirnov.devel@gmail.com>
2023-07-16 16:45:36 +0300

    chech CI

--- build_apk_release.sh
+++ build_apk_release.sh
@@ -1,5 +1,5 @@
-echo 'Starting apk release build'
+echo 'Starting developer build apk'
 fvm flutter clean
 fvm flutter pub get
 fvm flutter build apk --release --target lib/main_dev.dart
-echo 'apk release build done'
+echo 'apk developer build done'
